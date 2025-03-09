7976e6eebb6eb13c38b9d9659ec6ceabe8b616c7
xzwqq <sensovdiia@gmail.com>
2025-03-09 20:07:46 +0500

    qwe

---
+++ src/app/App.tsx
@@ -0,0 +1,18 @@
+import { unstable_HistoryRouter as HistoryRouter, Routes, Route } from 'react-router-dom';
+import { history } from './provider/history.ts';
+import Auth from "../pages/Auth/Auth.tsx";
+import "./style/App.css";
+
+function App() {
+  return (
+    <>
+      <HistoryRouter history={history}>
+        <Routes>
+          <Route path="/" element={<Auth />} />
+        </Routes>
+      </HistoryRouter>
+    </>
+  );
+}
+
+export default App;
